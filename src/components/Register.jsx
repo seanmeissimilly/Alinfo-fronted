e8b7bc84018cfd01a1995befea2e076bc7c90304
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-07-11 12:54:18 -0400

    arreglado lo de los ojos en contraseña de register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -67,11 +67,7 @@ export default function Register() {
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="w-full max-w-md space-y-8">
             <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src={u}
-                alt="Foto del Usuario"
-              />
+              <img className="mx-auto h-12 w-auto" src={u} alt="Your Company" />
               <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                 Registre su cuenta
               </h2>
@@ -86,71 +82,71 @@ export default function Register() {
               <input type="hidden" name="remember" defaultValue="true" />
               <div className="-space-y-px rounded-md shadow-sm">
                 <div className="my-8">
-                  <label htmlFor="user_name" className="sr-only">
-                    Nombre de usuario
-                  </label>
-                  <input
-                    value={user_name}
-                    onChange={(e) => setUser_name(e.target.value)}
-                    id="username"
-                    name="username"
-                    type="text"
-                    autoComplete="email"
-                    required
-                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Nombre de usuario"
-                  />
-                </div>
-
-                <div className="my-8">
-                  <label htmlFor="email-address" className="sr-only">
-                    Dirección de correo electrónico
-                  </label>
-                  <input
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    id="email-address"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Correo Electrónico"
-                  />
-                </div>
-
-                <div className="my-8">
-                  <div className=" mx-auto relative flex items-center">
-                    <label htmlFor="password" className="sr-only">
-                      Contraseña
+                  <div className="my-8">
+                    <label htmlFor="user-name" className="sr-only">
+                      Nombre de usuario
                     </label>
                     <input
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      id="password"
-                      name="password"
-                      type={openpassword ? "text" : "password"}
-                      autoComplete="current-password"
+                      value={user_name}
+                      onChange={(e) => setUser_name(e.target.value)}
+                      id="username"
+                      name="username"
+                      type="text"
+                      autoComplete="email"
                       required
-                      className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      placeholder="Contraseña"
+                      className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Nombre de usuario"
                     />
-                    <div className="text-2xl absolute right-3">
-                      {!openpassword ? (
-                        <AiFillEye onClick={handleshowpassword} />
-                      ) : (
-                        <AiFillEyeInvisible onClick={handleshowpassword} />
-                      )}
+                  </div>
+
+                  <div className="">
+                    <label htmlFor="email-address" className="sr-only">
+                      Dirección de correo electrónico
+                    </label>
+                    <input
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      id="email-address"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Correo Electrónico"
+                    />
+                  </div>
+
+                  <div className="my-8">
+                    <div className=" mx-auto relative flex items-center">
+                      <label htmlFor="password" className="sr-only">
+                        Contraseña
+                      </label>
+                      <input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        id="password"
+                        name="password"
+                        type={openpassword ? "text" : "password"}
+                        autoComplete="current-password"
+                        required
+                        className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Contraseña"
+                      />
+                      <div className="text-2xl absolute right-3">
+                        {!openpassword ? (
+                          <AiFillEye onClick={handleshowpassword} />
+                        ) : (
+                          <AiFillEyeInvisible onClick={handleshowpassword} />
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                <div className="my-8">
-                  <div className=" mx-auto relative flex items-center">
-                    <label htmlFor="password" className="sr-only">
-                      Confirmar Contraseña
-                    </label>
-                    <div className="w-full">
+                  <div className="my-8">
+                    <div className=" mx-auto relative flex items-center">
+                      <label htmlFor="confirmpassword" className="sr-only">
+                        Confirmar Contraseña
+                      </label>
                       <input
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
@@ -162,44 +158,44 @@ export default function Register() {
                         className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                         placeholder="Confirmar Contraseña"
                       />
+                      <div className="text-2xl absolute right-3">
+                        {!openconfirmpassword ? (
+                          <AiFillEye onClick={handleshowconfirmpassword} />
+                        ) : (
+                          <AiFillEyeInvisible
+                            onClick={handleshowconfirmpassword}
+                          />
+                        )}
+                      </div>
                     </div>
-                    <div className="text-2xl absolute right-3">
-                      {!openconfirmpassword ? (
-                        <AiFillEye onClick={handleshowconfirmpassword} />
-                      ) : (
-                        <AiFillEyeInvisible
-                          onClick={handleshowconfirmpassword}
-                        />
-                      )}
+                    <div className="my-8">
+                      <PasswordChecklist
+                        className=""
+                        rules={[
+                          "minLength",
+                          "specialChar",
+                          "number",
+                          "capital",
+                          "match",
+                          "lowercase",
+                          "notEmpty",
+                        ]}
+                        minLength={8}
+                        value={password}
+                        valueAgain={confirmPassword}
+                        messages={{
+                          minLength: "La contraseña tiene más de 8 caracteres.",
+                          specialChar:
+                            "La contraseña tiene caracteres especiales.",
+                          number: "La contraseña tiene un número.",
+                          capital: "La contraseña tiene una letra mayúscula.",
+                          match: "Las contraseñas coinciden.",
+                          lowercase: "La contraseña tiene una letra minúscula.",
+                          notEmpty: "La contraseña no está en blanco.",
+                        }}
+                        onChange={(e) => setIsValid(e)}
+                      />
                     </div>
-                  </div>
-                  <div className="my-8">
-                    <PasswordChecklist
-                      className=""
-                      rules={[
-                        "minLength",
-                        "specialChar",
-                        "number",
-                        "capital",
-                        "match",
-                        "lowercase",
-                        "notEmpty",
-                      ]}
-                      minLength={8}
-                      value={password}
-                      valueAgain={confirmPassword}
-                      messages={{
-                        minLength: "La contraseña tiene más de 8 caracteres.",
-                        specialChar:
-                          "La contraseña tiene caracteres especiales.",
-                        number: "La contraseña tiene un número.",
-                        capital: "La contraseña tiene una letra mayúscula.",
-                        match: "Las contraseñas coinciden.",
-                        lowercase: "La contraseña tiene una letra minúscula.",
-                        notEmpty: "La contraseña no está en blanco.",
-                      }}
-                      onChange={(e) => setIsValid(e)}
-                    />
                   </div>
                 </div>
               </div>
