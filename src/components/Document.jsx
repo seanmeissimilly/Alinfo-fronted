8fda22232504c5774001702acb5e70ed2d87278f
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-07-16 12:22:59 -0400

    creando Documents

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -1,35 +1,75 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FaEdit, FaTrash, FaDownload } from "react-icons/fa";
 
 const Document = ({
+  id,
   name,
   description,
   type,
   classification,
   user,
+  userImage,
   downloadLink,
+  userRole,
+  onDelete,
 }) => {
   return (
     <div className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden mb-4">
       <div className="p-6">
-        <div className="flex items-center justify-between">
-          <div className="text-sm font-medium text-indigo-500">{type}</div>
-          <a
-            href={downloadLink}
-            className="text-indigo-600 hover:text-indigo-900"
-          >
-            Descargar
-          </a>
-        </div>
         <h2 className="mt-2 text-lg font-semibold text-gray-900">{name}</h2>
-        <p className="mt-2 text-gray-600">{description}</p>
+        <p className="mt-3 text-gray-600 whitespace-pre-line">{description}</p>
         <div className="mt-4">
-          <span className="text-gray-700">Clasificación: </span>
-          <span className="text-gray-900 font-bold">{classification}</span>
-        </div>
-        <div className="mt-2">
-          <span className="text-gray-700">Subido por: </span>
-          <span className="text-gray-900 font-bold">{user}</span>
+          <div className="mt-1">
+            <span className="text-gray-700">Clasificación: </span>
+            <span className="text-gray-900 font-bold">{classification}</span>
+          </div>
+          <div className="mt-1">
+            <span className="text-gray-700">Tipo: </span>
+            <span className="text-gray-900 font-bold">{type}</span>
+          </div>
+          <div className="mt-4 flex items-center">
+            <img
+              src={userImage}
+              className="rounded-full mr-2"
+              width="40"
+              alt="User"
+            />
+            <span className="text-gray-700">Subido por: </span>
+            <span className="font-bold ml-2">{user}</span>
+          </div>
+          <div className="flex items-center justify-between mt-4">
+            <a
+              href={downloadLink}
+              className="text-indigo-600 hover:text-indigo-900 bg-indigo-100 hover:bg-indigo-200 px-3 py-1 rounded flex items-center"
+              download
+            >
+              <FaDownload className="mr-2" />
+              <span className="hidden group-hover:inline">Descargar</span>
+            </a>
+            {(userRole === "admin" || userRole === "editor") && (
+              <div className="flex space-x-4">
+                <a
+                  href={`/edit/${id}`}
+                  className="text-blue-600 hover:text-blue-900 relative group"
+                >
+                  <FaEdit />
+                  <span className="absolute bottom-full mb-1 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2">
+                    Editar
+                  </span>
+                </a>
+                <button
+                  onClick={onDelete}
+                  className="text-red-600 hover:text-red-900 relative group"
+                >
+                  <FaTrash />
+                  <span className="absolute bottom-full mb-1 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2">
+                    Borrar
+                  </span>
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -37,12 +77,16 @@ const Document = ({
 };
 
 Document.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   classification: PropTypes.string.isRequired,
   user: PropTypes.string.isRequired,
+  userImage: PropTypes.string.isRequired,
   downloadLink: PropTypes.string.isRequired,
+  userRole: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Document;
--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -1,101 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { documentList, documentDelete } from "../redux/documentSlice";
+import { userList } from "../redux/userSlice.js";
 import Document from "./Document";
 
 function Documents() {
-  const documents = [
-    {
-      name: "Documento PDF",
-      description: "Este es un documento PDF.",
-      type: "tesis",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.pdf",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-    {
-      name: "Documento Word",
-      description: "Este es un documento Word.",
-      type: "doctorado",
-      classification: "encapsulado",
-      user: "davidam",
-      downloadLink: "/path/to/document.docx",
-    },
-  ];
+  // Declaro la URL de la API en dependencia del entorno
+  const URL =
+    process.env.NODE_ENV === "production"
+      ? import.meta.env.VITE_BACKEND_URL
+      : "http://localhost:8000";
+  const dispatch = useDispatch();
+
+  const document = useSelector((state) => state.document);
+  const {
+    error: documentError,
+    loading: documentLoading,
+    documents,
+    success: documentSuccess,
+  } = document;
+
+  const user = useSelector((state) => state.user);
+  const { users, userInfo, success: userSuccess } = user;
+
+  useEffect(() => {
+    dispatch(documentList({ token: userInfo.token }));
+    dispatch(userList({ token: userInfo.token }));
+  }, [dispatch, userInfo, userSuccess, documentSuccess]);
+
+  const handleDelete = (id) => {
+    if (window.confirm("¿Estás seguro de que deseas borrar este documento?")) {
+      dispatch(documentDelete({ id, token: userInfo.token }));
+    }
+  };
+
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {documents.map((doc, index) => (
+        {documents.map((doc) => (
           <Document
-            key={index}
-            name={doc.name}
+            key={doc.id}
+            id={doc.id}
+            name={doc.title}
             description={doc.description}
             type={doc.type}
             classification={doc.classification}
             user={doc.user}
-            downloadLink={doc.link}
+            userImage={doc.userImage}
+            userRole={user.role}
+            downloadLink={doc.downloadLink}
+            onDelete={() => handleDelete(doc.id)}
           />
         ))}
       </div>
