ade0ddb76ea2c8a2e769bf4808d65bd4aa01ed88
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-07-16 21:09:59 -0400

    refactorizando más componentes

--- src/components/EditBlog.jsx
+++ src/components/EditBlog.jsx
@@ -71,102 +71,98 @@ export default function EditBlog() {
       ) : error ? (
         <Messages>{error}</Messages>
       ) : (
-        <div>
-          <div className="md:grid md:grid-cols-4 md:gap-6">
-            <div className="md:col-span-1"></div>
-            <div className="mt-5 md:col-span-2 md:mt-0">
-              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Actualizar Publicación
-              </h2>
+        <div className="md:grid md:grid-cols-4 md:gap-6">
+          <div className="md:col-span-1"></div>
+          <div className="mt-5 md:col-span-2 md:mt-0">
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Actualizar Publicación
+            </h2>
 
-              <form onSubmit={submitHandler}>
-                <div className="shadow sm:overflow-hidden sm:rounded-md">
-                  <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
-                    <div className="grid grid-cols-3 gap-6">
-                      <div className="col-span-3 sm:col-span-2"></div>
-                    </div>
+            <form onSubmit={submitHandler}>
+              <div className="shadow sm:overflow-hidden sm:rounded-md">
+                <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                  <div className="grid grid-cols-3 gap-6">
+                    <div className="col-span-3 sm:col-span-2"></div>
+                  </div>
 
-                    <div>
-                      <label
-                        htmlFor="about"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Título
-                      </label>
-                      <div className="mt-1">
-                        <input
-                          value={title}
-                          onChange={(e) => setTitle(e.target.value)}
-                          name="title"
-                          required
-                          type="text"
-                          id="title"
-                          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                          placeholder="Título de la publicación"
-                        />
-                      </div>
-                    </div>
-
-                    <div>
-                      <label
-                        htmlFor="about"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Temática
-                      </label>
-                      <div className="mt-1">
-                        <textarea
-                          value={body}
-                          onChange={(e) => setBody(e.target.value)}
-                          type="text"
-                          id="body"
-                          rows={3}
-                          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                          placeholder="Escribe aquí la publicación"
-                        />
-                      </div>
-                    </div>
-
-                    <div>
-                      <label
-                        htmlFor="image"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Imagen
-                      </label>
-                      <div className="mt-1">
-                        <input
-                          onChange={handleImageChange}
-                          name="image"
-                          type="file"
-                          id="image"
-                          accept="image/*"
-                          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        />
-                      </div>
-                      {imageUrl && (
-                        <div className="mt-4">
-                          <img
-                            src={imageUrl}
-                            //src={`${URL}${blogInfo.image}`}
-                            alt="Selected"
-                            className="h-48 w-full object-cover"
-                          />
-                        </div>
-                      )}
+                  <div>
+                    <label
+                      htmlFor="title"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Título
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        name="title"
+                        required
+                        type="text"
+                        id="title"
+                        className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Título de la publicación"
+                      />
                     </div>
                   </div>
-                  <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+
+                  <div>
+                    <label
+                      htmlFor="body"
+                      className="block text-sm font-medium text-gray-700"
                     >
-                      Editar
-                    </button>
+                      Temática
+                    </label>
+                    <div className="mt-1">
+                      <textarea
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                        id="body"
+                        rows={3}
+                        className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Escribe aquí la publicación"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="image"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Imagen
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        onChange={handleImageChange}
+                        name="image"
+                        type="file"
+                        id="image"
+                        accept="image/*"
+                        className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    {imageUrl && (
+                      <div className="mt-4">
+                        <img
+                          src={imageUrl}
+                          alt="Selected"
+                          className="h-48 w-full object-cover"
+                        />
+                      </div>
+                    )}
                   </div>
                 </div>
-              </form>
-            </div>
+                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Editar
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       )}
