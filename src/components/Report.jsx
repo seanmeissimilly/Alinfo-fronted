4f5c41a3199631ed533a9a62929f3a86ec8d3c93
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-07-20 15:10:54 -0400

    ya genero mi primer pdf

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,24 +1,29 @@
-import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { FaDownload } from "react-icons/fa";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 
 const Report = ({ name, columns, data, date }) => {
+  const generateReport = () => {
+    const report = new jsPDF();
+    report.text(name, 95, 20);
+    report.text(`Fecha: ${date}`, 10, 20);
+    report.save();
+  };
+
   return (
     <div className="max-w-xs mx-auto bg-white rounded-lg shadow-md overflow-hidden mb-2 flex flex-col justify-between">
       <div className="p-4 flex-grow">
         <h2 className="mt-1 text-md font-semibold text-gray-900">{name}</h2>
       </div>
       <div className="flex items-center justify-between p-4">
-        <a
-          href=""
+        <button
+          onClick={() => generateReport()}
           className="text-indigo-600 hover:text-indigo-900 bg-indigo-100 hover:bg-indigo-200 px-2 py-1 rounded flex items-center"
-          download
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <FaDownload className="mr-1" />
           <span className="hidden group-hover:inline">Descargar</span>
-        </a>
+        </button>
       </div>
     </div>
   );
--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,5 +1,57 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Messages from "./Messages.jsx";
+import Loader from "./Loader.jsx";
+import Report from "./Report.jsx";
+import { userList } from "../redux/userSlice.js";
+import moment from "moment";
+
 function Reports() {
-  return <div>Reports</div>;
+  const dispatch = useDispatch();
+
+  const { users, userInfo, error, loading } = useSelector(
+    (state) => state.user
+  );
+
+  useEffect(() => {
+    if (userInfo?.token) {
+      dispatch(userList({ token: userInfo.token }));
+    }
+  }, [dispatch, userInfo, error]);
+
+  const renderReports = () => {
+    // Ordeno los documentos por ID antes de renderizarlos
+
+    // return users.map((user) => {
+    return (
+      <Report
+        key={"user.id"}
+        name={"Listado de Usuarios"}
+        columns={"user.id"}
+        data={"user.id"}
+        date={moment().format("DD-MM-YYYY")}
+      />
+    );
+    // });
+  };
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Messages>{error}</Messages>
+      ) : (
+        <div>
+          <div className="container mx-auto p-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {renderReports()}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Reports;
