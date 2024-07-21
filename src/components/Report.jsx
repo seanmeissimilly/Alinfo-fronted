44504a55828c68aa7abf1529a49cd717f666a018
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-07-20 22:02:48 -0400

    logo de la Cujae más pequeño

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,24 +1,33 @@
 import PropTypes from "prop-types";
 import { FaDownload } from "react-icons/fa";
 import jsPDF from "jspdf";
+import logo_cujae from "../media/logo_cujae - reporte.png";
 import autoTable from "jspdf-autotable";
 
 const Report = ({ name, columns, data, date }) => {
   const generateReport = () => {
     const report = new jsPDF({ orientation: "landscape", format: "letter" });
-    report.text(name, 95, 20);
-    report.text(`Fecha: ${date}`, 10, 30);
+    report.setFontSize(14);
+    report.setFont("Calibri", "bold");
+    report.text("Alinfo", 12, 40);
+    report.setLanguage("es-ES");
+    report.addImage(logo_cujae, 5, 5);
+    report.text(name, 120, 40);
+    report.text(`Fecha: ${date}`, 225, 30);
 
     //todo: Generar la tabla
     report.autoTable({
-      // styles: { fillColor: [255, 0, 0] },
-      startY: 50,
+      startY: 60,
       head: columns,
       margin: { top: 10 },
       body: data,
       theme: "striped",
       fontStyle: "normal",
       font: "times",
+      pageBreak: "auto",
+      tableWidth: "auto",
+      rowPageBreak: "auto",
+      styles: { fontSize: 12, cellWidth: "auto", lineColor: 10 },
     });
 
     report.save(`${name} ${date}.pdf`);
