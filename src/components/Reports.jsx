5d7c8f41c5aff73aeef744166818eb6abefbd8b0
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-07-20 20:07:15 -0400

    instalar moment

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -14,6 +14,7 @@ import {
   documentclassificationList,
   documenttypesList,
 } from "../redux/documentSlice";
+import moment from "moment/moment.js";
 
 function Reports() {
   const dispatch = useDispatch();
