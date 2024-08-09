1f68eb4df581e52609e925be0eb5eb19161ac6bc
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-08-08 21:38:24 -0400

    Actualizando y borrando paquetes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
-/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
 
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 export default withMT({
-  content: ["./src/**/*.{html,js,jsx}"],
-  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-
     fontFamily: {
       sans: ["Open Sans", "sans-serif"],
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: ["@tailwindcss/forms"],
 });
