1504436053de4cd25a40a299eabc6401f763714f
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-08-11 17:00:56 -0400

    Mejorado Login y Register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,7 +8,16 @@ import user_icon from "../media/user.png";
 import { toast } from "react-hot-toast";
 import { userLogin } from "../redux/userSlice.js";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Input } from "@material-tailwind/react";
+import {
+  Input,
+  Card,
+  CardHeader,
+  Checkbox,
+  CardBody,
+  CardFooter,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -55,85 +64,68 @@ export default function Login() {
         <Loader />
       ) : (
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mb-10">
-          <div className="w-full max-w-md space-y-8">
-            <div>
+          <Card className="w-96">
+            <CardHeader
+              variant="gradient"
+              color="white"
+              className="mb-4 grid h-28 place-items-center"
+            >
               <img
                 className="mx-auto h-12 w-auto"
                 src={user_icon}
                 alt="CUJAE"
               />
-              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+
+              <Typography variant="h4" color="black">
                 Iniciar sesión en su cuenta
-              </h2>
-            </div>
-            <form
-              onSubmit={submitHandler}
-              className="mt-8 space-y-6"
-              action="#"
-              method="POST"
-            >
-              <input type="hidden" name="remember" defaultValue="true" />
-              <div className="space-y-px rounded-md shadow-sm">
-                <div className="my-8">
-                  <label htmlFor="email-address" className="sr-only">
-                    Dirección de correo electrónico
-                  </label>
+              </Typography>
+            </CardHeader>
+
+            <form onSubmit={submitHandler} action="#" method="POST">
+              <CardBody className="flex flex-col gap-4">
+                <Input
+                  label="Correo Electrónico"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Correo Electrónico"
+                  size="lg"
+                />
+                <div className="relative">
                   <Input
-                    label="Correo Electrónico"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    label="Contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    id="password"
+                    name="password"
+                    type={openpassword ? "text" : "password"}
+                    autoComplete="current-password"
                     required
-                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Correo Electrónico"
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Contraseña"
+                    size="lg"
                   />
-                </div>
-                <div className="my-8">
-                  <div className=" mx-auto relative flex items-center">
-                    <label htmlFor="password" className="sr-only">
-                      Contraseña
-                    </label>
-                    <Input
-                      label="Contraseña"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      id="password"
-                      name="password"
-                      type={openpassword ? "text" : "password"}
-                      autoComplete="current-password"
-                      required
-                      className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      placeholder="Contraseña"
-                    />
-                    <div className="text-2xl absolute right-3">
-                      {!openpassword ? (
-                        <AiFillEye onClick={handleshowpassword} />
-                      ) : (
-                        <AiFillEyeInvisible onClick={handleshowpassword} />
-                      )}
-                    </div>
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-2xl">
+                    {!openpassword ? (
+                      <AiFillEye onClick={handleshowpassword} />
+                    ) : (
+                      <AiFillEyeInvisible onClick={handleshowpassword} />
+                    )}
                   </div>
                 </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="text-sm">
-                  <a
-                    href="/register"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Registrarse
-                  </a>
+                <div className="-ml-2.5">
+                  <Checkbox label="Recúerdame" />
                 </div>
-              </div>
-
-              <div>
-                <button
+              </CardBody>
+              <CardFooter className="pt-0">
+                <Button
                   type="submit"
-                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 normal-case"
                 >
                   <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                     <LockClosedIcon
@@ -142,10 +134,25 @@ export default function Login() {
                     />
                   </span>
                   Entrar
-                </button>
-              </div>
+                </Button>
+                <Typography
+                  variant="small"
+                  className="mt-6 flex justify-center"
+                >
+                  ¿No tienes una cuenta?
+                  <Typography
+                    as="a"
+                    href="/register"
+                    variant="small"
+                    color="blue-gray"
+                    className="ml-1 font-bold"
+                  >
+                    Registrarse
+                  </Typography>
+                </Typography>
+              </CardFooter>
             </form>
-          </div>
+          </Card>
         </div>
       )}
     </>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -9,16 +9,24 @@ import { toast } from "react-hot-toast";
 import { userRegister } from "../redux/userSlice.js";
 import PasswordChecklist from "react-password-checklist";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Input } from "@material-tailwind/react";
+import {
+  Input,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 
 export default function Register() {
   const [user_name, setUser_name] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [isValid, setIsValid] = useState(false); // Estado inicial: no válido
+  const [isValid, setIsValid] = useState(false);
   const [openpassword, setOpenPassword] = useState(false);
-  const [openconfirmpassword, setOpenConfirmPassword] = useState(false); //Para controlar cuando se vea la contraseña.
+  const [openconfirmpassword, setOpenConfirmPassword] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -66,164 +74,125 @@ export default function Register() {
         <Loader />
       ) : (
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mb-10">
-          <div className="w-full max-w-md space-y-8">
-            <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src={user_icon}
-                alt="Your Company"
-              />
-              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Registre su cuenta
-              </h2>
-            </div>
-
-            <form
-              onSubmit={submitHandler}
-              className="mt-8 space-y-6 "
-              action="#"
-              method="POST"
+          <Card className="w-96">
+            <CardHeader
+              variant="gradient"
+              color="white"
+              className="mb-4 grid h-28 place-items-center"
             >
-              <input type="hidden" name="remember" defaultValue="true" />
-              <div className="-space-y-px rounded-md shadow-sm">
+              <img className="mx-auto h-12 w-auto" src={user_icon} alt="" />
+              <Typography variant="h4" color="black">
+                Registre su cuenta
+              </Typography>
+            </CardHeader>
+
+            <form onSubmit={submitHandler} action="#" method="POST">
+              <CardBody className="flex flex-col gap-4">
+                <Input
+                  label="Nombre de Usuario"
+                  value={user_name}
+                  onChange={(e) => setUser_name(e.target.value)}
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Nombre de usuario"
+                  size="lg"
+                />
+
+                <Input
+                  label="Correo Electrónico"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Correo Electrónico"
+                  size="lg"
+                />
+                <div className="relative">
+                  <Input
+                    label="Contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    id="password"
+                    name="password"
+                    type={openpassword ? "text" : "password"}
+                    autoComplete="current-password"
+                    required
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Contraseña"
+                    size="lg"
+                  />
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-2xl">
+                    {!openpassword ? (
+                      <AiFillEye onClick={handleshowpassword} />
+                    ) : (
+                      <AiFillEyeInvisible onClick={handleshowpassword} />
+                    )}
+                  </div>
+                </div>
+
+                <div className="relative">
+                  <Input
+                    label="Confirmar Contraseña"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    id="confirmpassword"
+                    name="password"
+                    type={openconfirmpassword ? "text" : "password"}
+                    autoComplete="current-password"
+                    required
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Confirmar Contraseña"
+                    size="lg"
+                  />
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-2xl">
+                    {!openconfirmpassword ? (
+                      <AiFillEye onClick={handleshowconfirmpassword} />
+                    ) : (
+                      <AiFillEyeInvisible onClick={handleshowconfirmpassword} />
+                    )}
+                  </div>
+                </div>
+
                 <div className="my-8">
-                  <div className="my-8">
-                    <label htmlFor="user-name" className="sr-only">
-                      Nombre de usuario
-                    </label>
-                    <Input
-                      label="Nombre de Usuario"
-                      value={user_name}
-                      onChange={(e) => setUser_name(e.target.value)}
-                      id="username"
-                      name="username"
-                      type="text"
-                      autoComplete="email"
-                      required
-                      className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      placeholder="Nombre de usuario"
-                    />
-                  </div>
-
-                  <div className="">
-                    <label htmlFor="email-address" className="sr-only">
-                      Dirección de correo electrónico
-                    </label>
-                    <Input
-                      label="Correo Electrónico"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      id="email-address"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      placeholder="Correo Electrónico"
-                    />
-                  </div>
-
-                  <div className="my-8">
-                    <div className=" mx-auto relative flex items-center">
-                      <label htmlFor="password" className="sr-only">
-                        Contraseña
-                      </label>
-                      <Input
-                        label="Contraseña"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        id="password"
-                        name="password"
-                        type={openpassword ? "text" : "password"}
-                        autoComplete="current-password"
-                        required
-                        className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="Contraseña"
-                      />
-                      <div className="text-2xl absolute right-3">
-                        {!openpassword ? (
-                          <AiFillEye onClick={handleshowpassword} />
-                        ) : (
-                          <AiFillEyeInvisible onClick={handleshowpassword} />
-                        )}
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="my-8">
-                    <div className=" mx-auto relative flex items-center">
-                      <label htmlFor="confirmpassword" className="sr-only">
-                        Confirmar Contraseña
-                      </label>
-                      <Input
-                        label="Confirmar Contraseña"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        id="confirmpassword"
-                        name="password"
-                        type={openconfirmpassword ? "text" : "password"}
-                        autoComplete="current-password"
-                        required
-                        className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="Confirmar Contraseña"
-                      />
-                      <div className="text-2xl absolute right-3">
-                        {!openconfirmpassword ? (
-                          <AiFillEye onClick={handleshowconfirmpassword} />
-                        ) : (
-                          <AiFillEyeInvisible
-                            onClick={handleshowconfirmpassword}
-                          />
-                        )}
-                      </div>
-                    </div>
-                    <div className="my-8">
-                      <PasswordChecklist
-                        className=""
-                        rules={[
-                          "minLength",
-                          "specialChar",
-                          "number",
-                          "capital",
-                          "match",
-                          "lowercase",
-                          "notEmpty",
-                        ]}
-                        minLength={8}
-                        value={password}
-                        valueAgain={confirmPassword}
-                        messages={{
-                          minLength: "La contraseña tiene más de 8 caracteres.",
-                          specialChar:
-                            "La contraseña tiene caracteres especiales.",
-                          number: "La contraseña tiene un número.",
-                          capital: "La contraseña tiene una letra mayúscula.",
-                          match: "Las contraseñas coinciden.",
-                          lowercase: "La contraseña tiene una letra minúscula.",
-                          notEmpty: "La contraseña no está en blanco.",
-                        }}
-                        onChange={(e) => setIsValid(e)}
-                      />
-                    </div>
-                  </div>
+                  <PasswordChecklist
+                    className=""
+                    rules={[
+                      "minLength",
+                      "specialChar",
+                      "number",
+                      "capital",
+                      "match",
+                      "lowercase",
+                      "notEmpty",
+                    ]}
+                    minLength={8}
+                    value={password}
+                    valueAgain={confirmPassword}
+                    messages={{
+                      minLength: "La contraseña tiene más de 8 caracteres.",
+                      specialChar: "La contraseña tiene caracteres especiales.",
+                      number: "La contraseña tiene un número.",
+                      capital: "La contraseña tiene una letra mayúscula.",
+                      match: "Las contraseñas coinciden.",
+                      lowercase: "La contraseña tiene una letra minúscula.",
+                      notEmpty: "La contraseña no está en blanco.",
+                    }}
+                    onChange={(e) => setIsValid(e)}
+                  />
                 </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="text-sm">
-                  <a
-                    href="/login"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Entrar
-                  </a>
-                </div>
-              </div>
-
-              <div>
-                <button
+              </CardBody>
+              <CardFooter className="pt-0">
+                <Button
                   type="submit"
-                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 normal-case"
                 >
                   <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                     <LockClosedIcon
@@ -232,10 +201,25 @@ export default function Register() {
                     />
                   </span>
                   Registrarse
-                </button>
-              </div>
+                </Button>
+                <Typography
+                  variant="small"
+                  className="mt-6 flex justify-center"
+                >
+                  ¿Ya tienes una cuenta?
+                  <Typography
+                    as="a"
+                    href="/login"
+                    variant="small"
+                    color="blue-gray"
+                    className="ml-1 font-bold"
+                  >
+                    Entrar
+                  </Typography>
+                </Typography>
+              </CardFooter>
             </form>
-          </div>
+          </Card>
         </div>
       )}
     </>
