fba7c97f7ec4bd2c13a4c245cbb60cb47737e49e
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-08-13 14:15:42 -0400

    Creando los test.

--- src/test/Footer.test.jsx
+++ src/test/Footer.test.jsx
@@ -1,28 +1,36 @@
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
-import Footer from "./Footer";
-import { MemoryRouter } from "react-router-dom";
+import { describe, expect, test, beforeEach } from "vitest";
+import Footer from "../components/Footer";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
+
+const customTheme = {
+  component: {
+    Typography: {
+      as: "a",
+      color: "blue-gray",
+    },
+  },
+};
 
 describe("Footer Component", () => {
-  it("renders Footer component with correct text", () => {
+  beforeEach(() => {
     render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
+      <BrowserRouter>
+        <ThemeProvider value={customTheme}>
+          <Footer />
+        </ThemeProvider>
+      </BrowserRouter>
     );
+  });
 
+  test("renders Footer component with correct text", () => {
     expect(screen.getByText("© 2024 Alinfo")).toBeInTheDocument();
     expect(screen.getByText("Ayuda")).toBeInTheDocument();
     expect(screen.getByText("Acerca de")).toBeInTheDocument();
   });
 
-  it("contains links with correct href attributes", () => {
-    render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    );
-
+  test("contains links with correct href attributes", () => {
     expect(screen.getByText("Ayuda").closest("a")).toHaveAttribute(
       "href",
       "/help"
@@ -33,13 +41,7 @@ describe("Footer Component", () => {
     );
   });
 
-  it("applies correct styles to the footer", () => {
-    render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    );
-
+  test("applies correct styles to the footer", () => {
     const footer = screen.getByRole("contentinfo");
     expect(footer).toHaveClass(
       "fixed inset-x-0 bottom-0 w-full bg-gray-400 p-3"
--- src/test/Landing.test.jsx
+++ src/test/Landing.test.jsx
@@ -1,4 +1,4 @@
-import { describe, test, expect, beforeEach, it } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Landing from "../components/Landing";
 
@@ -11,9 +11,28 @@ describe("Landing", () => {
       screen.getByText("Repositorio Virtual de Ingeniería Alimentaria.")
     ).toBeDefined();
   });
-  it("Should show the component", () => {
-    expect(
-      screen.getByText("Repositorio Virtual de Ingeniería Alimentaria.")
-    ).toBeDefined();
+  test("renders the main heading", () => {
+    const heading = screen.getByText(/AlInfo/i);
+    expect(heading).toBeInTheDocument();
+  });
+  test("renders the first paragraph", () => {
+    const paragraph1 = screen.getByText(
+      /Repositorio Virtual de Ingeniería Alimentaria./i
+    );
+    expect(paragraph1).toBeInTheDocument();
+  });
+
+  test("renders the second paragraph", () => {
+    const paragraph2 = screen.getByText(
+      /Facultad de Ingeniería Química, Cujae./i
+    );
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  test("renders the third paragraph", () => {
+    const paragraph3 = screen.getByText(
+      /Universidad Tecnológica de la Habana./i
+    );
+    expect(paragraph3).toBeInTheDocument();
   });
 });
