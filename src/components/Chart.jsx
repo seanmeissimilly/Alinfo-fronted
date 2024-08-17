4c34a276aa4b01b08b6a5de292216a4c6f16ac20
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-08-17 18:44:59 -0400

    Terminado gráfico de pastel con filtros.

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -7,10 +7,24 @@ import { appList } from "../redux/appSlice.js";
 import { documentList } from "../redux/documentSlice";
 import { suggestionList } from "../redux/suggestionSlice.js";
 import Messages from "./Messages.jsx";
+import { DateTime } from "luxon";
+import {
+  Input,
+  Checkbox,
+  Card,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
 
 function Chart() {
   const dispatch = useDispatch();
-  const [filter, setFilter] = useState("lastMonth");
+  const [filter, setFilter] = useState("noFilter");
+  const [customRange, setCustomRange] = useState({ start: "", end: "" });
+  const [showUsers, setShowUsers] = useState(true);
+  const [showDocuments, setShowDocuments] = useState(true);
+  const [showVideos, setShowVideos] = useState(true);
+  const [showTools, setShowTools] = useState(true);
+  const [showSuggestions, setShowSuggestions] = useState(true);
 
   const {
     users,
@@ -42,13 +56,53 @@ function Chart() {
     }
   }, [dispatch, userInfo]);
 
+  const filterData = (data, filter, dateField = "date") => {
+    const now = DateTime.now();
+    let startDate;
+    let endDate = now;
+
+    switch (filter) {
+      case "lastMonth":
+        startDate = now.minus({ months: 1 });
+        break;
+      case "lastYear":
+        startDate = now.minus({ years: 1 });
+        break;
+      case "lastWeek":
+        startDate = now.minus({ weeks: 1 });
+        break;
+      case "Today":
+        startDate = now.startOf("day");
+        break;
+      case "customRange":
+        startDate = DateTime.fromISO(customRange.start);
+        endDate = DateTime.fromISO(customRange.end);
+        break;
+      case "noFilter":
+        return data;
+      default:
+        startDate = DateTime.fromISO("1970-01-01");
+    }
+
+    return data.filter((item) => {
+      const itemDate = DateTime.fromISO(item[dateField]);
+      return itemDate >= startDate && itemDate <= endDate;
+    });
+  };
+
+  const filteredUsers = filterData(users, filter, "start_date");
+  const filteredDocuments = filterData(documents, filter);
+  const filteredVideos = filterData(videos, filter);
+  const filteredTools = filterData(tools, filter);
+  const filteredSuggestions = filterData(suggestions, filter);
+
   const data = [
     ["Apartado", "Cantidad"],
-    ["Usuarios", users?.length],
-    ["Documentos", documents?.length],
-    ["Videos", videos?.length],
-    ["Herramientas", tools?.length],
-    ["Quejas y Sugerencias", suggestions?.length],
+    ["Usuarios", showUsers ? filteredUsers?.length : 0],
+    ["Documentos", showDocuments ? filteredDocuments?.length : 0],
+    ["Videos", showVideos ? filteredVideos?.length : 0],
+    ["Herramientas", showTools ? filteredTools?.length : 0],
+    ["Quejas y Sugerencias", showSuggestions ? filteredSuggestions?.length : 0],
   ];
 
   const options = {
@@ -59,7 +113,13 @@ function Chart() {
 
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
-    // Aquí puedes agregar la lógica para filtrar los datos según la fecha seleccionada
+  };
+
+  const handleCustomRangeChange = (e) => {
+    setCustomRange({
+      ...customRange,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
@@ -71,26 +131,95 @@ function Chart() {
       errorSuggestion ? (
         <Messages>{errorUser}</Messages>
       ) : (
-        <div className="flex flex-row  justify-center h-screen bg-gray-100">
-          <div className="w-4/5 h-3/4 p-4 m-2 bg-white rounded-lg shadow-lg my-10 ml-2">
+        <div className="flex flex-col lg:flex-row justify-center h-screen bg-gray-100">
+          <div className="w-full lg:w-4/5 h-3/4 p-4 bg-white rounded-lg shadow-lg my-10 mx-2">
             <PieChart data={data} options={options} />
           </div>
-          <div className="w-1/5 h-1/3 md:w-1/2 md:h-1/4 p-4 m-2 bg-white rounded-lg shadow-lg flex items-center justify-center my-10">
-            <div className="flex flex-col items-center">
-              <label className="mb-2">Filtro</label>
+          <Card className="w-full lg:w-1/4 h-3/4 p-4 m-2 bg-white rounded-lg shadow-lg flex items-center justify-center my-10">
+            <CardBody>
+              <Typography
+                variant="h5"
+                color="blue-gray"
+                className="mb-2 text-center"
+              >
+                Filtro
+              </Typography>
+
               <select
                 value={filter}
                 onChange={handleFilterChange}
-                className="p-2 border rounded"
+                className="w-72 p-2 border rounded mb-4"
               >
-                <option value="lastMonth">Último mes</option>
+                <option value="noFilter">Sin filtro</option>
                 <option value="lastYear">Último año</option>
+                <option value="lastMonth">Último mes</option>
                 <option value="lastWeek">Última semana</option>
                 <option value="Today">Hoy</option>
                 <option value="customRange">Rango personalizado</option>
               </select>
-            </div>
-          </div>
+
+              {filter === "customRange" && (
+                <div className="flex flex-row my-2 space-x-2">
+                  <div className="flex flex-col">
+                    <Input
+                      type="date"
+                      name="start"
+                      label="Fecha Inicial"
+                      value={customRange.start}
+                      onChange={handleCustomRangeChange}
+                      className="p-2 border rounded mb-2"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <Input
+                      type="date"
+                      name="end"
+                      label="Fecha Final"
+                      value={customRange.end}
+                      onChange={handleCustomRangeChange}
+                      className="p-2 border rounded"
+                    />
+                  </div>
+                </div>
+              )}
+              <div className="flex flex-col items-start mt-4">
+                <Checkbox
+                  label="Usuarios"
+                  checked={showUsers}
+                  onChange={() => setShowUsers(!showUsers)}
+                  className="mr-2"
+                />
+
+                <Checkbox
+                  label="Documentos"
+                  checked={showDocuments}
+                  onChange={() => setShowDocuments(!showDocuments)}
+                  className="mr-2"
+                />
+
+                <Checkbox
+                  label="Videos"
+                  checked={showVideos}
+                  onChange={() => setShowVideos(!showVideos)}
+                  className="mr-2"
+                />
+
+                <Checkbox
+                  label="Herramientas"
+                  checked={showTools}
+                  onChange={() => setShowTools(!showTools)}
+                  className="mr-2"
+                />
+
+                <Checkbox
+                  label="Quejas y Sugerencias"
+                  checked={showSuggestions}
+                  onChange={() => setShowSuggestions(!showSuggestions)}
+                  className="mr-2"
+                />
+              </div>
+            </CardBody>
+          </Card>
         </div>
       )}
     </>
