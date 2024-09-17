5506d81c88e520b87e328802ccda1436335b8dad
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-09-17 06:45:59 -0400

    Creado servidor con Vite.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Permite el acceso desde cualquier IP
+    port: 5173,
+  },
   test: {
     environment: "jsdom",
     name: "jsdom",
