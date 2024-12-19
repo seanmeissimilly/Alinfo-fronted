4aade14b26cdcab8d4a37c85af1f03e0a41b26ff
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-12-19 09:52:00 -0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://seanmeissimilly.github.io/Alinfo-fronted",
   server: {
     host: "0.0.0.0", // Permite el acceso desde cualquier IP
     port: 5173,
